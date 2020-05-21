a3873fa622ffc39fca90eed3a4737d0836ca98da
Moskalenko Nikita <n.moskalneko1996@yahoo.com>
2020-05-22 02:33:06 +0300

    Add profile view controller

---
+++ MyApp/AddInformationProfile/Controller/AddInformationProfile.swift
@@ -0,0 +1,30 @@
+//
+//  AddInformationProfile.swift
+//  MyApp
+//
+//  Created by Nikita Moskalenko on 5/22/20.
+//  Copyright © 2020 Nikita Moskalenko. All rights reserved.
+//
+
+import UIKit
+
+class AddInformationProfile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
